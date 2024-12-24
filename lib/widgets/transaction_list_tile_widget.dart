ea5f47a95f4371475b983740726be837d63d2f46
johnstef99 <john@johnstef.com>
2024-12-24 15:13:49 +0200

    feature: show tooltip with transaction comment

--- lib/widgets/transaction_list_tile_widget.dart
+++ lib/widgets/transaction_list_tile_widget.dart
@@ -31,59 +31,63 @@ class TransactionListTile extends StatelessWidget {
         ),
     ];
 
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: ListTile(
-        contentPadding:
-            EdgeInsets.only(left: 16, right: actions.isEmpty ? 16 : 0),
-        key: ValueKey('transaction_${transaction.id}'),
-        title: Text(
-          transaction.contact?.displayName ?? tr('deleted_contact'),
-          style: TextStyle(
-            color: transaction.contact == null ? Colors.grey : Colors.black,
-          ),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              DateFormat('d MMM yyyy HH:mm', context.locale.toLanguageTag())
-                  .format(
-                transaction.transactionAt.toLocal(),
-              ),
+    return Tooltip(
+      triggerMode: TooltipTriggerMode.tap,
+      message: transaction.comment ?? '',
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: ListTile(
+          contentPadding:
+              EdgeInsets.only(left: 16, right: actions.isEmpty ? 16 : 0),
+          key: ValueKey('transaction_${transaction.id}'),
+          title: Text(
+            transaction.contact?.displayName ?? tr('deleted_contact'),
+            style: TextStyle(
+              color: transaction.contact == null ? Colors.grey : Colors.black,
             ),
-            if (transaction.comment != null)
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                transaction.comment!,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  color: Colors.grey.shade800,
-                  fontWeight: FontWeight.w300,
+                DateFormat('d MMM yyyy HH:mm', context.locale.toLanguageTag())
+                    .format(
+                  transaction.transactionAt.toLocal(),
                 ),
               ),
-          ],
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              transaction.amount.toCurrency(context.locale),
-              style: TextStyle(
-                color: switch (transaction.amount) {
-                  > 0 => Colors.green,
-                  < 0 => Colors.red,
-                  _ => Colors.grey,
-                },
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+              if (transaction.comment?.isNotEmpty == true)
+                Text(
+                  transaction.comment!,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+            ],
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                transaction.amount.toCurrency(context.locale),
+                style: TextStyle(
+                  color: switch (transaction.amount) {
+                    > 0 => Colors.green,
+                    < 0 => Colors.red,
+                    _ => Colors.grey,
+                  },
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
-            ),
-            if (actions.isNotEmpty)
-              PopupMenuButton(
-                  icon: const Icon(Icons.more_vert),
-                  itemBuilder: (context) => actions),
-          ],
+              if (actions.isNotEmpty)
+                PopupMenuButton(
+                    icon: const Icon(Icons.more_vert),
+                    itemBuilder: (context) => actions),
+            ],
+          ),
         ),
       ),
     );
