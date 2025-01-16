0160798d44bf68fd2c2b58865348fce6c4944d4c
johnstef99 <john@johnstef.com>
2025-01-16 18:17:19 +0200

    chore: use local server in dev mode

--- mobile/lib/pocketbase.dart
+++ mobile/lib/pocketbase.dart
@@ -1,1 +1,5 @@
-const pocketbaseUrl = String.fromEnvironment('POCKETBASE_URL');
+import 'package:flutter/foundation.dart';
+
+const pocketbaseUrl = kReleaseMode
+    ? String.fromEnvironment('POCKETBASE_URL')
+    : 'http://127.0.0.1:8090';
